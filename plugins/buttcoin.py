5a687d226635041d5fdb3367dffd0c09cf45d862
James Ward <james@notjam.es>
2015-06-13 14:42:10 -0700

    fix bad encodings in buttcoin plugin

--- plugins/buttcoin.py
+++ plugins/buttcoin.py
@@ -2,6 +2,7 @@ import random
 import re
 from time import strptime, strftime
 from urllib import quote
+import unicodedata
 
 from util import http, hook
 
@@ -16,8 +17,13 @@ def buttcoin(inp, api_key=None):
     ".bitcoin -- gets current exchange rate for bitcoins from BTC-e"
     data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
 
-    data['ticker']['buttcoin'] = twitter.twitter("buttcoin", api_key)
+    buttcoin = twitter.twitter("buttcoin", api_key)
 
+    buttcoin = unicodedata.normalize('NFKD', buttcoin)
+    buttcoin = buttcoin.encode('ascii', 'replace')
+    
+    data['ticker']['buttcoin'] = buttcoin
+    
     return (
         "USD/BTC: \x0307{buy:.0f}\x0f - High: \x0307{high:.0f}\x0f"
         " - Low: \x0307{low:.0f}\x0f - Volume: {vol_cur:.0f}"
