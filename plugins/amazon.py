86a814dfe66e8b18dc234e0c04f5d723b19c5134
Parker Mauney <parkermauney+git@gmail.com>
2017-08-04 17:25:42 -0400

    Removed URL parsing

--- plugins/amazon.py
+++ plugins/amazon.py
@@ -1,10 +1,7 @@
-import re
 import bottlenose
 import bitly_api
 from lxml import etree
 from util import hook, http
-
-amazon_re = (r'amazon.com/([\\w-]+/)?(dp|gp/product)/(\\w+/)?(\\w{10})', re.I)
 
 @hook.api_key('amazon')
 @hook.command('amazon')
@@ -39,25 +36,3 @@ def amazon(inp, api_key=None):
     shortUrl = bitlyResponse['url']
 
     return title + ": " + shortUrl
-
-@hook.api_key('amazon')
-@hook.regex(*amazon_re)
-def amazon_url(match, say=None, api_key=None):
-    if api_key is None or 'AWS_KEY' not in api_key or 'SECRET_KEY' not in api_key or 'ASSOCIATE_TAG' not in api_key:
-        return "missing API key"
-    url = match.group(0)
-    index = 0
-    id = url[index+3:]
-
-    amazon = bottlenose.Amazon(api_key['AWS_KEY'], api_key['SECRET_KEY'], api_key['ASSOCIATE_TAG'], Region='US')
-    result = amazon.ItemLookup(id)
-
-    for item in result.Items.Item:
-        description = item.ItemAttributes.Title + " - "
-
-    price = api.item_lookup(id, ResponseGroup='Offers')
-
-    for a in price.Items.Item.Offers.Offer:
-        description += a.OfferListing.Price.FormattedPrice
-
-    say(description)
