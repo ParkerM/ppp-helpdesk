aa939b0855b2fd2230fd333403d7316dc65c6b07
James Ward <james@notjam.es>
2017-08-05 12:16:15 -0400

    include price for amazon plugin

--- plugins/amazon.py
+++ plugins/amazon.py
@@ -10,7 +10,8 @@ def amazon(inp, api_key=None):
        return "missing API key"
 
     amazon = bottlenose.Amazon(api_key['access_key'], api_key['secret_key'], api_key['associate_tag'], Region='US')
-    response = amazon.ItemSearch(Keywords=inp, SearchIndex='All')
+
+    response = amazon.ItemSearch(Keywords=inp, SearchIndex='All', ResponseGroup='ItemAttributes,Offers')
 
     NS = '{http://webservices.amazon.com/AWSECommerceService/2013-08-01}'
 
@@ -23,9 +24,16 @@ def amazon(inp, api_key=None):
 
     item = root.find('.//' + NS + 'Item')  # First item in results
     asin = item.find(NS + 'ASIN').text
-    url = item.find(NS + 'DetailPageURL').text
 
     itemAttributes = item.find(NS + 'ItemAttributes')
     title = itemAttributes.find(NS + 'Title').text
 
-    return "%s - https://amzn.com/%s?tag=%s" % (title, asin, api_key['associate_tag'])
+    try:
+      new_price = item.find(NS + 'OfferSummary/' + NS + 'LowestNewPrice/' + NS + 'FormattedPrice').text
+    except:
+      new_price = None
+
+    if new_price:
+      return "\x02%s\x02 - New \x02%s\x02 - https://amzn.com/%s?tag=%s" % (title, new_price, asin, api_key['ASSOCIATE_TAG'])
+    else:
+      return "\x02%s\x02 - https://amzn.com/%s?tag=%s" % (title, asin, api_key['ASSOCIATE_TAG'])
