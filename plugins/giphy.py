60efa5bbab4463714df8dd93c1c7c606bee4dbaf
James Ward <james@notjam.es>
2017-10-05 13:23:23 -0400

    add giphy plugin

---
+++ plugins/giphy.py
@@ -0,0 +1,13 @@
+from util import http, hook
+
+@hook.api_key('giphy')
+@hook.command('giphy', autohelp=False)
+@hook.command('gif', autohelp=False)
+@hook.command(autohelp=False)
+def giphy(inp, api_key=None):
+    ".giphy [term] -- gets random gif for a term"
+
+    data = http.get_json("http://api.giphy.com/v1/gifs/random", { "api_key": api_key, "tag": inp })
+
+    return data['data']['image_url']
+
